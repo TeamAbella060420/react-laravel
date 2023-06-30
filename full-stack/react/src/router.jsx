f24f5cead7e2820f77ab87d52b5556624e78b32d
TeamAbella060420 <teamabella@outlook.com>
2023-06-30 13:56:16 +0800

    add code

---
+++ full-stack/react/src/router.jsx
@@ -0,0 +1,12 @@
+import {createBrowserRouter} from 'react-router-dom';
+import React from 'react'
+
+const router = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default router
