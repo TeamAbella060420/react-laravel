a1850870cc481ed2bacb5ec34a3a0055ed554172
TeamAbella060420 <teamabella@outlook.com>
2023-07-01 16:16:30 +0800

    Update Login.jsx

--- full-stack/react/src/views/Login.jsx
+++ full-stack/react/src/views/Login.jsx
@@ -10,6 +10,7 @@ export default function Login() {
   return (
     <div className='login-signup-form animated fadeInDown'>
       <div className="form">
+        <h1 className='title'>Login into your account</h1>
         <form onSubmit={onSubmit}>
           <input type="email" placeholder='Email'/>
           <input type="password" placeholder='Password'/>
