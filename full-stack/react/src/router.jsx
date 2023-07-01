664edb34c430c3b1807ae4f23670e732a0559be9
TeamAbella060420 <teamabella@outlook.com>
2023-07-01 12:18:29 +0800

    Update router.jsx

--- full-stack/react/src/router.jsx
+++ full-stack/react/src/router.jsx
@@ -1,21 +1,46 @@
-import {createBrowserRouter} from 'react-router-dom';
+import {createBrowserRouter, Navigate} from 'react-router-dom';
 import Login from './views/login';
 import Signup from './views/Signup';
 import Users from './views/Users';
+import DefaultLayout from './components/DefaultLayout';
+import GuestLayout from './components/GuestLayout';
+import Dashboard from './views/Dashboard';
 
 const router = createBrowserRouter([
     {
-        path: '/login',
-        element: <Login/>
+        path: '/',
+        element: <GuestLayout/>,
+        children: [
+            {
+                path: '/login',
+                element: <Login/>
+            },
+            {
+                path: '/signup',
+                element: <Signup/>
+            },
+        ]
     },
+
     {
-        path: '/signup',
-        element: <Signup/>
+        path: '/',
+        element: <DefaultLayout/>,
+        children: [
+            {
+                path: '/',
+                element: <Navigate to ="/users"/>
+            },
+            {
+                path: '/users',
+                element: <Users/>
+            },
+            {
+                path: '/dashboard',
+                element: <Dashboard/>
+            },
+        ]
     },
-    {
-        path: '/users',
-        element: <Users/>
-    },
+
 ]) 
 
 export default router
