1b24986551449742a0d99cc40076bbbd314bdf68
TeamAbella060420 <teamabella@outlook.com>
2023-07-01 21:11:12 +0800

    Update LoginRequest.php

--- full-stack/app/Http/Requests/LoginRequest.php
+++ full-stack/app/Http/Requests/LoginRequest.php
@@ -13,7 +13,7 @@ class LoginRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,10 @@ class LoginRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'email' => 'required|email|exists:users,email',
+            'password' => [
+                'required',
+            ]
         ];
     }
 }
