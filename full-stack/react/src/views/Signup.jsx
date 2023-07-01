cfb3f745bce95cc8daaaec1006fff9ba9b64da37
TeamAbella060420 <teamabella@outlook.com>
2023-07-01 17:56:16 +0800

    Update Signup.jsx

--- full-stack/react/src/views/Signup.jsx
+++ full-stack/react/src/views/Signup.jsx
@@ -1,5 +1,7 @@
 import React, { useRef } from 'react'
 import { Link } from 'react-router-dom';
+import axiosClient from '../axios-client';
+import { useStateContext } from '../context/ContextProvider';
 
 export default function Signup() {
   const nameRef = useRef();
@@ -7,14 +9,25 @@ export default function Signup() {
   const passwordRef = useRef();
   const passwordConfirmationRef = useRef();
 
+  const {setUser, setToken} = useStateContext();
+
   const onSubmit = (e) => {
     e.preventDefault();
     const payload = {
-      name : nameRef.current.vale,
+      name : nameRef.current.value,
       email : emailRef.current.value,
       password: passwordRef.current.value,
-      passwordConfirmation: passwordConfirmationRef.current.value
+      password_confirmation: passwordConfirmationRef.current.value
     }
+    axiosClient.post('/signup', payload).then(({data}) => {
+      setToken(data.token);
+      setUser(data.user)
+    }).catch(err => {
+      const response = err.response;
+      if(response && response.status === 422) {
+        console.log(response.data.errors);
+      }
+    })
   }
 
 
