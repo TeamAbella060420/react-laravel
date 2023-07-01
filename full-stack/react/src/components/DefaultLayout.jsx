b518585b88c88be46310fae669809de97664fa3f
TeamAbella060420 <teamabella@outlook.com>
2023-07-01 13:10:23 +0800

    Update DefaultLayout.jsx

--- full-stack/react/src/components/DefaultLayout.jsx
+++ full-stack/react/src/components/DefaultLayout.jsx
@@ -1,20 +1,28 @@
-import React from 'react'
-import { Outlet, Navigate, Link } from 'react-router-dom';
-import { useStateContext } from '../context/ContextProvider';
+import React from "react";
+import { Outlet, Navigate, Link } from "react-router-dom";
+import { useStateContext } from "../context/ContextProvider";
 
 export default function DefaultLayout() {
     const { user, token } = useStateContext();
-    if(!token) {
-        return <Navigate to="/login" />
+    if (!token) {
+        return <Navigate to="/login" />;
     }
 
-  return (
-    <div id='defaultLayout'>
-      <aside>
-      <Link to='/dashboard'>Dashbord</Link>
-      <Link to='/users'>Users</Link>
-      </aside>
-      <Outlet />
-    </div>
-  )
+    return (
+        <div id="defaultLayout">
+            <aside>
+                <Link to="/dashboard">Dashbord</Link>
+                <Link to="/users">Users</Link>
+            </aside>
+            <div className="content">
+                <header>
+                    <div>Header</div>
+                    <div>User info</div>
+                </header>
+                <main>
+                    <Outlet />
+                </main>
+            </div>
+        </div>
+    );
 }
