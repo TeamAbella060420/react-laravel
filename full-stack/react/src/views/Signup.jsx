f579e956d2b524efc6d00a1f2936d3352752b636
TeamAbella060420 <teamabella@outlook.com>
2023-07-01 17:25:58 +0800

    Update Signup.jsx

--- full-stack/react/src/views/Signup.jsx
+++ full-stack/react/src/views/Signup.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Link } from 'react-router-dom';
 
 export default function Signup() {
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmationRef = useRef();
 
   const onSubmit = (e) => {
     e.preventDefault();
+    const payload = {
+      name : nameRef.current.vale,
+      email : emailRef.current.value,
+      password: passwordRef.current.value,
+      passwordConfirmation: passwordConfirmationRef.current.value
+    }
   }
 
 
@@ -13,10 +23,10 @@ export default function Signup() {
     <div className="form">
       <h1 className='title'>Signup for free</h1>
       <form onSubmit={onSubmit}>
-        <input type="text" placeholder='Full Name'/>
-        <input type="email" placeholder='Email'/>
-        <input type="password" placeholder='Password'/>
-        <input type="password" placeholder='Confirm Password'/>
+        <input ref={nameRef} type="text" placeholder='Full Name'/>
+        <input ref={emailRef} type="email" placeholder='Email'/>
+        <input ref={passwordRef} type="password" placeholder='Password'/>
+        <input ref={passwordConfirmationRef} type="password" placeholder='Confirm Password'/>
         <button className='btn btn-block'>Sign up</button>
         <p className='message'>
           Already Registered? <Link to='/login'>Sign in</Link>
