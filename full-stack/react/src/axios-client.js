b6597a2df7acd47335297f3320860a68551e9250
TeamAbella060420 <teamabella@outlook.com>
2023-07-01 17:11:43 +0800

    Update axios-client.js

--- full-stack/react/src/axios-client.js
+++ full-stack/react/src/axios-client.js
@@ -4,4 +4,16 @@ const axiosClient = axios.create({
     baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`
 })
 
+axiosClient.interceptors.request.use((config) => {
+    const token = localStorage.get('ACCESS_TOKEN')
+    config.headers.Authorization = `Bearer ${token}`
+    return config;
+})
+
+axiosClient.interceptors.response.use((response) => {
+    return response;
+}, () => {
+
+})
+
 export default axiosClient;
